7b93a72e4b0b78067500dd4d19b0902b46146931
Martin Lozano <martin.lozano995@gmail.com>
2023-12-18 22:34:15 -0300

    insert simple firts commit

--- ProyectoAplicacion/Form1.cs
+++ ProyectoAplicacion/Form1.cs
@@ -46,5 +46,11 @@ namespace ProyectoAplicacion
                 pbxPokemon.Load("https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg");
             }
         }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            frmAltaPokemon alta = new frmAltaPokemon();
+            alta.ShowDialog();
+        }
     }
 }
